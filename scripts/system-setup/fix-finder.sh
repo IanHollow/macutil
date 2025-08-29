bb192a00221fbfbf8a5ca367e36d7ac2e88aa62c
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-08-29 13:03:54 -0400

    Remove sidebar icon size setting & allow iCloud
    The sidebar icon size also changes the icon size in the system settings. I do not mind the size change in finder however the font size becomes a lot smaller in the system settings side bar and that makes it a lot harder to read.
    
    I feel that removing iCloud by default is a little harsh and should be moved a separate script. Apple is all about ecosystem so while some people might not use iCloud disabling this ecosystem feature by default is a very opinionated move.

--- scripts/system-setup/fix-finder.sh
+++ scripts/system-setup/fix-finder.sh
@@ -39,10 +39,6 @@ fixfinder () {
     printf "%b\n" "${CYAN}Showing all filename extensions in Finder...${RC}"
     $ESCALATION_TOOL defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-    # Set the sidebar icon size to small
-    printf "%b\n" "${CYAN}Setting sidebar icon size to small...${RC}"
-    $ESCALATION_TOOL defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
-
     # Show status bar in Finder
     printf "%b\n" "${CYAN}Showing status bar in Finder...${RC}"
     $ESCALATION_TOOL defaults write com.apple.finder ShowStatusBar -bool true
@@ -55,8 +51,7 @@ fixfinder () {
     printf "%b\n" "${CYAN}Cleaning up Finder's sidebar...${RC}"
     $ESCALATION_TOOL defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
     $ESCALATION_TOOL defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
-    $ESCALATION_TOOL defaults write com.apple.finder SidebarShowingiCloudDesktop -bool false
-
+    
     # Restart Finder to apply changes
     printf "%b\n" "${GREEN}Finder has been restarted and settings have been applied.${RC}"
     $ESCALATION_TOOL killall Finder
